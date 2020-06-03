16025bab058ce88d8e36501efbb764eb2ee00d8e
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2020-06-03 15:28:18 +0200

    Upgrade Rails from 6.0.2.2 to 6.0.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_143921) do
+ActiveRecord::Schema.define(version: 2020_06_03_132118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
