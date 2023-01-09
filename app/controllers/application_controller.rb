766b1904b18f3fa7740bcdd47ffb4cd8dc55fdeb
Michele Gerarduzzi <hello@michelegera.dev>
2023-01-09 15:12:18 +0100

    Add support for multiple locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,21 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  around_action :set_locale
+
+  def set_locale(&)
+    [params[:locale], extract_locale, I18n.default_locale].each do |locale|
+      if locale && I18n.available_locales.index(locale.to_sym)
+        I18n.locale = locale
+        break
+      end
+    end
+
+    I18n.with_locale(locale, &)
+  end
+
+  def extract_locale
+    request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+  end
 end
